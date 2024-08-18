ac988e93da443af398be9456911c9ef371845963
Ayush Pandey <ayushpandey9350@gmail.com>
2024-08-18 23:36:02 +0530

    fixed ui and added client profile creation

--- src/components/ui/input.jsx
+++ src/components/ui/input.jsx
@@ -1,13 +1,14 @@
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
+import "../../app/globals.css"
 
 const Input = React.forwardRef(({ className, type, ...props }, ref) => {
   return (
     (<input
       type={type}
       className={cn(
-        "flex h-9 w-full rounded-md border border-neutral-200 bg-transparent px-3 py-1  text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-neutral-950 disabled:cursor-not-allowed disabled:opacity-50 dark:border-neutral-800 dark:placeholder:text-neutral-400 dark:focus-visible:ring-neutral-300",
+        "flex h-9 font-inter font-medium w-full  border-b border-stroke bg-transparent px-0 py-1  text-sm  transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:font-inter placeholder:font-medium  placeholder:text-neutral-500     focus-visible:ring-neutral-950 disabled:cursor-not-allowed disabled:opacity-50  dark:placeholder:text-neutral-400 dark:focus-visible:ring-neutral-300 focus:border-waterloo focus:placeholder:text-black focus-visible:outline-none dark:border-strokedark dark:focus:border-manatee dark:focus:placeholder:text-white",
         className
       )}
       ref={ref}
