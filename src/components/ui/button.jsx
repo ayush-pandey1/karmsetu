87e36d8f53d8808f7d1a9ab4e7f4286c4ba9e805
Ayush Pandey <ayushpandey9350@gmail.com>
2024-08-27 11:20:25 +0530

    Application page and added application modal in job detail page

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -20,6 +20,7 @@ const buttonVariants = cva(
           "bg-neutral-100 text-neutral-900 shadow-sm hover:bg-neutral-100/80 dark:bg-neutral-800 dark:text-neutral-50 dark:hover:bg-neutral-800/80",
         ghost: "hover:bg-neutral-100 hover:text-neutral-900 dark:hover:bg-neutral-800 dark:hover:text-neutral-50",
         link: "text-neutral-900 underline-offset-4 hover:underline dark:text-neutral-50",
+        accept: "bg-green-500 hover:bg-green-600 focus:bg-green-500 text-white",
       },
       size: {
         default: "h-9 px-4 py-2",
--- src/components/ui/dialog.jsx
+++ src/components/ui/dialog.jsx
@@ -53,7 +53,7 @@ const DialogHeader = ({
   ...props
 }) => (
   <div
-    className={cn("flex flex-col space-y-1.5 text-center sm:text-left", className)}
+    className={cn("flex flex-col space-y-1.5  sm:text-left", className)}
     {...props} />
 )
 DialogHeader.displayName = "DialogHeader"
