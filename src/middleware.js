a6b12051c22fec1f44029d8612e0adaecaa46ad6
suraj2004ok <surajsingh2004ok@gmail.com>
2024-09-17 04:37:21 +0530

    blocked all middlewares

--- src/middleware.js
+++ src/middleware.js
@@ -8,25 +8,25 @@ export async function middleware(req) {
   //console.log('Middleware triggered for:', req.nextUrl.pathname);
 
   // If user is not logged in and tries to access protected routes, redirect to /auth/signin
-  if (!token && pathname.startsWith('/cl') || !token &&  pathname.startsWith('/fl')) {
-    return NextResponse.redirect(new URL('/auth/signin', req.url));
-  }
+  // if (!token && pathname.startsWith('/cl') || !token &&  pathname.startsWith('/fl')) {
+  //   return NextResponse.redirect(new URL('/auth/signin', req.url));
+  // }
 
   // If the user is logged in and tries to access signin or signup, redirect based on role
-  if (token && (pathname === '/auth/signin' ||  pathname === '/auth/signup')) {
-    const redirectUrl = token?.role === 'client' ? '/cl' : '/fl';
-    return NextResponse.redirect(new URL(redirectUrl, req.url));
-  }
+  // if (token && (pathname === '/auth/signin' ||  pathname === '/auth/signup')) {
+  //   const redirectUrl = token?.role === 'client' ? '/cl' : '/fl';
+  //   return NextResponse.redirect(new URL(redirectUrl, req.url));
+  // }
 
   // If user is a client and trying to access freelancer routes, redirect them to client dashboard
-  if (token?.role === 'client' && pathname.startsWith('/fl')) {
-    return NextResponse.redirect(new URL('/cl', req.url));
-  }
+  // if (token?.role === 'client' && pathname.startsWith('/fl')) {
+    // return NextResponse.redirect(new URL('/cl', req.url));
+  // }
 
   // If user is a freelancer and trying to access client routes, redirect them to freelancer dashboard
-  if (token?.role === 'freelancer' && pathname.startsWith('/cl')) {
-    return NextResponse.redirect(new URL('/fl', req.url));
-  }
+  // if (token?.role === 'freelancer' && pathname.startsWith('/cl')) {
+  //   return NextResponse.redirect(new URL('/fl', req.url));
+  // }
 
   return NextResponse.next();
 }
