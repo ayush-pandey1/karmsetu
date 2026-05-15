da0baa1b3d8320842eb1cfb499a969d80a14ba08
Ayush Pandey <116146717+ayush-pandey1@users.noreply.github.com>
2026-05-15 13:09:45 +0530

    Update contact address in Contact component

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -98,7 +98,7 @@ const Contact = () => {
                 <h3 className="mb-4 text-metatitle3 font-medium text-black dark:text-white">
                   Our Loaction
                 </h3>
-                <p>290 Maryam Springs 260, Courbevoie, Paris, France</p>
+                <p>BCIIT, Kalkaji</p>
               </div>
               <div className="5 mb-7">
                 <h3 className="mb-4 text-metatitle3 font-medium text-black dark:text-white">
